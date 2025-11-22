c47bc60d1455aae1656a344531068e5b801eeab4
Khakindra Devkota <writetokhakindra@gmail.com>
2025-11-22 19:32:28 +0530

    configuration_Changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-};
-
-module.exports = nextConfig;
+    output: 'export',  // enables static export
+  };
+  module.exports = nextConfig;
+  
