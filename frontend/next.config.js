66d30f6a69cf3d7b0990f24f0aec48428bdfa21a
Khakindra Devkota <writetokhakindra@gmail.com>
2025-11-30 17:09:20 +0530

    next js config changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+/** @type {import("next").NextConfig} */ 
+const nextConfig = 
+{ 
+  output: 'export', // enables static export 
+}; 
 
 module.exports = nextConfig;
